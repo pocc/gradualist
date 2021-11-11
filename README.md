110ae248777ee27818c291ab1e544294ef87de8a
Ross Jacobs <rj@swit.sh>
2021-11-11 15:48:22 -0800

    Refactored gradualism.py so it wasnt a gigantic main

--- README.md
+++ README.md
@@ -2,22 +2,34 @@
 
 *Gradually automate your procedures, one step at a time*
 
-Inspired by https://blog.danslimmon.com/2019/07/15/do-nothing-scripting-the-key-to-gradual-automation/comment-page-1/#comments
+Inspired by https://blog.danslimmon.com/2019/07/15/ 
 
 ## Features
 
 ### Main Features
 
-* Converts a markdown file into an interactive todo list
+* Converts a markdown file into interactive steps
 * Saves results into another markdown file
-* On completion, provides % automated. 
+* On 100% automated, saves results into a script file instead
 
-### Syntax
+### Keywords
 
-1. Variables
-    1. Store `{words}` in variables like so.
+Start steps with these words to use them.
+Using these words also makes it clearer what you want your user to do.
+
+1. Enter: *single line answer*
+    Synonyms that trigger: Type, Write, Provide,
+    1. Store `{response}` in variables like so.
     2. In steps after previous, `{words}` will be replaced whatever you entered.
-2. Run code
+    3. Input is entered on a single line
+    4. Automatically triggered if there is a `{variable}`
+2. Describe: *longform answer*
+    Synonyms that trigger: Explain
+    1. Store multiline `{response}` in variables like so.
+    2. In steps after previous, `{response}` will be replaced whatever you entered.
+    3. Type done on a newline to signify the end of input
+3. Run: *runs code*
+    Synonyms that trigger: Exec, Execute
     1. Run this inline `echo shell script`
         * Start the step with the word 'run', case insensitive
         * This will run in your shell, whichever that is for your OS.
@@ -33,6 +45,32 @@ Inspired by https://blog.danslimmon.com/2019/07/15/do-nothing-scripting-the-key-
         * `-I` is an arg to python. Include as many options as you want.
         * If an output file is specified with curly braces, it will be saved in
             the working directory; otherwise a temporary file will be used. 
+4. While: *Loops until condition*
+    Synonyms that trigger: Loop
+    * Format is 'While condition do task'
+    * While condition, keep on doing it 
+5. If: Conditional
+    Synonym for else: Otherwise, elsewise
+    Synonym for then: First comma
+    * Format is 'if condition then task1 else task2'
+    * Example: "if it's sunny, then have a picnic else read a book inside"
+7. Click: *Have the user click somewhere*
+    * This can be automated with clicking with x,y coordinates
+    * Use the browser extension to select the element
+8. Select
+    * Select from multiple options, usually in a dropdown
+    * Use the browser extension to select the element
+9. Open: Open a file or URL
+    * If opening a URL, default browser will be used
+    * Include the words 'with program' to specify which program to open it with (i.e. firefox
+      instead of chrome)
+    * Include the word 'new' to create a new one if one doesn't exist
+10. Transclude filepath:task list
+    * Include a separate markdown procedure as a sublist of the current task
+12. *Anything Else*
+    * The point of this is to be as compatible with as many procedures as
+      possible, so don't require keywords 
+    * There won't be any suggestions for automation 
 
 ### Defaults
 * Saved files have tasks with 
--- gradualism.py
+++ gradualism.py
@@ -54,95 +54,110 @@ def parse_md():
     return scripts
 
 
+def parse_step(step, user_vars, completed_steps):
+    was_task_completed = True
+
+    for v in user_vars:  # Replace future occurences of user vars
+        step = step.replace('{' + v + '}', user_vars[v])
+    var_matches = re.search('{(.*)}.*?', step)
+    shell_command = re.search(r'`\$ ?([^`]+)`', step)
+    code_block = re.search(r'```(\w+)([\s\S]+?)```', step)
+    step_number_match = re.match(r"(\d+\.)", step)
+    step_num = "1."
+    if step_number_match:
+        step_num = step_number_match[1]
+    if var_matches:
+        step = step.replace('{', '').replace('}', '')
+        if "personal" in var_matches[1]:
+            value = getpass.getpass(f"\n{step} ")
+        else:
+            value = input(f"\n{step} ")
+        user_vars[var_matches[1]] = value
+    elif code_block:
+        language, code = code_block[1], code_block[2]
+        code_lines = code.count('\n')
+        ans = input(f"{step_num} Run {code_lines} lines of {language} [y/n]? ")
+        if ans.startswith("y"):
+            cmd = [language, "donothing_code"]
+            try:
+                with open("donothing_code", 'w') as f:
+                    f.write(code)
+                child = sp.run(cmd, stdout=sp.PIPE, stderr=sp.PIPE)
+                output = (child.stdout + child.stderr).decode()
+                tabbed_output = output.replace('\n', '\n    ')
+                print(f"    Received\n    `{tabbed_output}`")
+                os.remove("donothing_code")
+            except Exception as e:
+                print("Got exception when trying to run code: ", e)
+        elif "optional" not in step.lower():
+            was_task_completed = False
+    elif shell_command:
+        cmd = shell_command[1]
+        # replace shell vars if they are env vars
+        shell_vars = re.findall(r"\$([a-zA-Z_]+)", step)
+        for sv in shell_vars:
+            env_sv = os.getenv(sv)
+            if env_sv:
+                cmd = cmd.replace('$' + sv, env_sv)
+
+        shell = os.getenv("SHELL")
+        if not shell:
+            shell = "your shell"
+        ans = input(f"{step_num} {cmd}\n\n    Run this in {shell} [y/n]? ")
+        if ans.startswith("y"):
+            try:
+                child = sp.run(cmd.split(' '), stdout=sp.PIPE, stderr=sp.PIPE)
+                output = (child.stdout + child.stderr).decode()
+                tabbed_output = output.replace('\n', '\n    ')
+                print(f"    Received\n    `{tabbed_output}`")
+            except Exception as e:
+                print("Got exception when trying to run code: ", e)
+        elif "optional" not in step.lower():
+            was_task_completed = False
+    elif "optional" in step.lower():
+        ans = input(f"""
+{step}
+    ENTER => continue  |  s => skip  |  b => go back 1 """)
+        if not ans.startswith("y"):
+            was_task_completed = False
+        elif ans.startswith("s"):
+            print("Skipping this step...")
+    else:
+        ans = input(f"""
+{step}
+    ENTER => continue  |  b => go back 1 """)
+        if not ans.startswith("y"):
+            was_task_completed = False
+    if was_task_completed:
+        completed_steps.append(step + "\n    [✔] " + get_dt_now() + '\n')
+    else:
+        completed_steps.append(step + "\n    [✘] " + get_dt_now() + '\n')
+    return completed_steps
+
+
 def main():
     sections = parse_md()
     for section in sections:
+        section_str = section + '\n' + '=' * len(section)
+        print(section_str)
+
         user_vars = {}
         completed_steps = []
-        section_str = section + '\n' + '='*len(section)
-        print(section_str)
         for step in sections[section]:
-            was_task_completed = True
-
-            for v in user_vars: # Replace future occurences of user vars
-                step = step.replace('{{' + v + '}}', user_vars[v])
-            var_matches = re.search('{{(.*)}}.*?', step)
-            shell_command = re.search('`\$ ?([^`]+)`', step)
-            code_block = re.search('```(\w+)([\s\S]+?)```', step)
-            step_number = re.match(r"(\d+\.)", step)[1]
-            if var_matches:
-                step = step.replace('{{', '').replace('}}', '')
-                if "personal" in var_matches[1]:
-                    value = getpass.getpass(f"\n{step} ")
-                else:
-                    value = input(f"\n{step} ")
-                user_vars[var_matches[1]] = value
-            elif code_block:
-                language, code = code_block[1], code_block[2]
-                code_lines = code.count('\n')
-                ans = input(f"{step_number} Run {code_lines} lines of {language} [y/n]? ")
-                if ans.startswith("y"): 
-                    cmd = [language, "donothing_code"]
-                    try:
-                        with open("donothing_code", 'w') as f:
-                            f.write(code)
-                        child = sp.run(cmd, stdout=sp.PIPE, stderr=sp.PIPE)
-                        output = (child.stdout + child.stderr).decode()
-                        tabbed_output = output.replace('\n', '\n    ')
-                        print(f"    Received\n    `{tabbed_output}`")
-                        os.remove("donothing_code")
-                    except Exception as e:
-                        print("Got exception when trying to run code: ", e)
-                elif "optional" not in step.lower():
-                    was_task_completed = False
-            elif shell_command:
-                cmd = shell_command[1]
-                # replace shell vars if they are env vars
-                shell_vars = re.findall(r"\$([a-zA-Z_]+)", step)
-                for sv in shell_vars:
-                    if os.getenv(sv):
-                        cmd = cmd.replace('$' + sv, os.getenv(sv))
-
-                shell = os.getenv("SHELL")
-                if not shell:
-                    shell = "your shell"
-                ans = input(f"{step_number} {cmd}\n\n    Run this in {shell} [y/n]? ")
-                if ans.startswith("y"): 
-                    try:
-                        child = sp.run(cmd.split(' '), stdout=sp.PIPE, stderr=sp.PIPE)
-                        output = (child.stdout + child.stderr).decode()
-                        tabbed_output = output.replace('\n', '\n    ')
-                        print(f"    Received\n    `{tabbed_output}`")
-                    except Exception as e:
-                        print("Got exception when trying to run code: ", e)
-                elif "optional" not in step.lower():
-                    was_task_completed = False
-            elif "optional" in step.lower():
-                ans = input('\n' + step + "\n\n    y => continue  |  s => skip  |  b => go back 1")
-                if not ans.startswith("y"): 
-                    was_task_completed = False
-                elif ans.startswith("s"): 
-                    print("Skipping this step...")
-            else:
-                ans = input('\n' + step + "\n\n    y => continue  |  b => go back 1")
-                if not ans.startswith("y"): 
-                    was_task_completed = False
-            if was_task_completed:
-                completed_steps.append(step + "\n    [✔] " + get_dt_now() + '\n') 
-            else:
-                completed_steps.append(step + "\n    [✘] " + get_dt_now() + '\n') 
+            completed_steps += parse_step(step, user_vars, completed_steps)
         section_words = section.split(' ', 1)[1]
         sect_date = section_words + get_dt_now()
         frontmatter = "---"
         for key in user_vars:
             if "personal" not in key:
-                frontmatter += f"\n{key}: {user_vars[key]}" 
+                frontmatter += f"\n{key}: {user_vars[key]}"
         frontmatter += "\n---\n"
-        sanitized_section = re.sub(r"[^A-Za-z0-9-_]", "", sect_date.replace(' ', '_'))
+        sect_date = sect_date.replace(' ', '_')
+        sanitized_section = re.sub(r"[^A-Za-z0-9-_]", "", sect_date)
         completed_steps = [section_str + '\n'] + completed_steps
         with open(sanitized_section + ".md", 'w') as f:
             content = frontmatter + '\n'.join(completed_steps)
             f.write(content)
 
-main()
 
+main()
