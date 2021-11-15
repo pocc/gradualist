649b5af5cf995f15dd72669da8fd6619f9177bc4
Ross Jacobs <rj@swit.sh>
2021-11-15 11:15:51 -0800

    Indented while results

--- src/gradualist.py
+++ src/gradualist.py
@@ -255,7 +255,7 @@ def parse_step(step, user_vars, completed_steps, QUIET_FLAG):
                 loop_resp = input(f"        {key}: ")
             if not log_data_keys:
                 loop_resp = input("        ")
-            resp += loop_resp + '\n'
+            resp += '    ' + str(loop_num) + '.' + loop_resp + '\n'
             loop_num += 1
         paragraphs.append(resp)
         print(f"\n    {start}      Started")
